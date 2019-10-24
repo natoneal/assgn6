3163849bc08de29b47b16ccb344fed573ed3d369
natoneal <nschade@andrew.cmu.edu>
2019-10-23 21:05:46 -0400

    a

---
+++ main.js
@@ -0,0 +1,26 @@
+var top;
+var amount;
+var price;
+
+
+
+
+
+function toppingImage() {
+    if (top === "none") {
+
+    }
+
+    if (top === "choco") {
+
+    }
+
+    if (top === "nilla") {
+
+    }
+
+    if (top === "sugar") {
+
+    }
+
+}
