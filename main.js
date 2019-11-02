acb5516bbba56e15da3192b6db5d810879eefd6f
natoneal <nschade@andrew.cmu.edu>
2019-11-02 18:41:19 -0400

    update of 6B javascript for populating cart

--- main.js
+++ main.js
@@ -37,25 +37,6 @@ function cartAmount() {
     amount = document.getElementById("amount").value;
     //set string to integer
     amount = parseInt(amount);
-// if (amt == 1) {
-//     amount = 1;
-//     console.log('1')
-//     }
-
-//     if (amt == 3) {
-//         amount=3;
-//         console.log('3')
-//     }
-
-//     if (amt == 6) {
-//         amount = 6;
-//         console.log('6')
-//     }
-
-//     if (amt == 12) {
-//         amount = 12;
-//         console.log('12')
-//     }
 
 }
 
@@ -67,17 +48,18 @@ function addToCart() {
 
     var cartObject = new cartItem(glaze, amount);
 
+//check if local storage is empty in order to the add the cart object
     if (localStorage.getItem("shoppingCartArray") != null){
-        shoppingCartArray = JSON.parse(localStorage.getItem("shoppingCartArray"));
+        shoppingCartArray = JSON.parse(localStorage.getItem("shoppingCartArray")); //change back to array
     }
-
+//
     shoppingCartArray.push(cartObject);
-    localStorage.setItem("shoppingCartArray", JSON.stringify(shoppingCartArray));
+    localStorage.setItem("shoppingCartArray", JSON.stringify(shoppingCartArray)); //change to string
     console.log(localStorage.getItem("shoppingCartArray"));
 
 }
 
-//create new obj.
+//create new obj. template
 function cartItem(glaze, amount){
     this.glaze = glaze;
     this.amount = amount;
@@ -92,61 +74,8 @@ function Checkout() {
 function onload(){
     shoppingCartArray = JSON.parse(localStorage.getItem("shoppingCartArray"));
 
+
 }
 
-//create new object
-// var cartItem = {toppingSelect:glaze, amtSelect:amount};
 
-    // cartItem.glaze1 = document.getElementById("selectTopping").value;
-    // cartItem.amount1 = document.getElementById("amount").value;
-
-    // document.getElementById("addtoCart").innerHTML = cartItem.glaze1e + cartItem.amount1;
-
-
-// function cartItem(addtoCart){
-//     this.glaze = glaze;
-//     console.log(glaze)
-//     this.amount1 = amt;
-//     console.log(amt)
-//     var Obj = document.theform.Obj.value;
-//     this.returnObj = function(){
-//         return Obj;
-//     }
-// };
-
-
-
-
-// document.getElementById("demo").innerHTML = cartItem.toppingSelect + cartItem.amtSelect;
-// function cartItem(topping) {
-//     amt: amt;
-//     this.topping = document.getElementById("selectTopping").value;
-// }
-
-// cartItem.prototype = {
-//     getInfo: function() {
-//       return 'A ' + this.color + ' ' + this.desc + '.';
-//     },
-//     drive: function() {
-//       //DO SOMETHING
-//     },
-//     stop: function() {
-//       //DO SOMETHING
-//     }
-// };
-
-// function cartItem(glaze, amount) {
-//         console.log(enter);
-//         glaze = (document.getElementById("selectTopping").value);
-//                 console.log(glaze);
-//         amount = document.getElementById("amount").value;
-//                 console.log(amount);
-//         return cartItem();
-//         // console.log(cartItem),
-// }
-
-// Object.create(cartItem());
-
-// var shoppingCart = [];
-// function displayShoppingCart (){};
 
